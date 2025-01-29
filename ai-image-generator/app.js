3f0b71ea3a6484f725b00fcfde71bb63cf54f26b
Shubham <shubhambhosale65867@gmail.com>
2025-01-29 19:27:13 +0530

    First Commit

---
+++ ai-image-generator/app.js
@@ -0,0 +1,42 @@
+const generateForm = document.querySelector('.generate_form');
+const imageGallery = document.querySelector('.image_gallery');
+const OPENAI_API_KEY = "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"
+
+const generateAiImages = async (userPrompt, userImageQuanmtity) => {
+    try {
+        const response = await fetch("https://api.openai.com/v1/images/generations", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${OPENAI_API_KEY}`,
+            },
+            body: JSON.stringify({
+                prompt: userPrompt,
+                n: parseInt(userImageQuanmtity),
+                size: "1024x1024",
+                response_format: "b64_json"
+            })
+        });
+        const { data } = await response.json();
+        console.log(data)
+    }
+    catch (error) {
+
+    }
+}
+
+const handleFormSubmission = (e) => {
+    e.preventDefault();
+    const userPrompt = e.srcElement[0].value;
+    const userImageQuanmtity = e.srcElement[1].value;
+
+    const imgMarkup = Array.from({ length: userImageQuanmtity }, () =>
+        ` <div class="img_card">
+        <a class="download_btn" href="#">Download</a>
+        <img src="assets/images/testimg.png" alt="">
+    </div>`
+    ).join("");
+    imageGallery.innerHTML = imgMarkup;
+    generateAiImages(userPrompt, userImageQuanmtity);
+}
+generateForm.addEventListener('submit', handleFormSubmission);
